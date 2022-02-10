288b6c6ef3e5fbaa77ecfcfae042c598b1c2d32e
Deng2020 <59889549+Deng2020@users.noreply.github.com>
2022-02-10 11:49:15 -0500

    Update Untitled2.m

--- Untitled2.m
+++ Untitled2.m
@@ -15,14 +15,14 @@ h = zeros(M,N);
 for i = 1:M
     for j = 1:N
         d = ((i-m_mid)^2+(j-n_mid)^2);
-        h(i,j) = exp(-(d)/(2*(d0^2))); 
+        h(i,j) = exp(-(d)/(2*(d0^2)));
     end
 end
 
 img_lpf = h.*img_f;
 
 img_lpf=ifftshift(img_lpf);    
-img_lpf=uint8(real(ifft2(img_lpf)));  
+img_lpf=uint8(real(ifft2(img_lpf)));
 
 subplot(1,2,1);imshow(image);title('original');
 subplot(1,2,2);imshow(img_lpf);title('d=50');
